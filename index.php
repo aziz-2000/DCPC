b9406d2fdadb2f8d07ce344535d343fbc0e17a41
Aziz <105892837+aziz-2000@users.noreply.github.com>
2024-07-01 06:51:16 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="ar">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="author" content="templatemo">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <title>  مسابقة ظفار للبرمجة</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-liberty-market.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+  </head>
+
+<body>
+
+  
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/ss.png" alt="">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                      
+                        <li><a href="#"> الصفحة الرئيسية </a></li>
+                        <li><a href="#">منصة  </a></li>
+                        <li><a href="./assets/images/DCPC Booklet.pdf">كتيب المسابقة </a></li>
+                    
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <!-- ***** Main Banner Area Start ***** -->
+  <div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class=" align-self-center">
+          <div class="header-text">
+           
+            <h2>أهلا بكم في مسابقة ظفار للبرمجة ٢٠٢٤</h2>
+            <p> أجواء مليئة بالتحديات المميزة والمنافسة الحماسية </p>
+            
+          </div>
+        </div>
+           
+            </div>
+      </div>
+    </div>
+  </div>
+<!--القسم الاول-->
+<section class="section">
+  <div class="cont">
+          <div class="image-text">
+              <img src="assets/images/img.png" alt="وصف الصورة">
+              <p>
+              <b>البرمجة التنافسية في سطور :</b><br>
+                البرمجة التنافسية في جوهرها عبارة عن لعبة ذهنية يتعين على المبرمج فيها كتابة
+                الشيفرات ضمن معطيات محددة مسبًقا.<br>
+            
+                <b>الهدف:</b><br>
+                تطوير مهارات البرمجة، وتنمية التفكير اإلبداعي والتفكير الناقد، وتعزيز القدرات
+                واإلمكانات على حل المشكالت، مستندة إلى استخدام مكثف وموسع للخوارزميات
+                وهياكل البيانات، وفهم عميق لألفكار الرياضية.<br>
+                
+              </p>
+          </div>
+      </div>
+      </section>      
+      <section class="sup">
+      <h2>الجهات المنظمة </h2>
+      <center><img src="assets/images/sup.png" alt=""><center>
+      </section>   
+ <!--القسم الثاني-->     
+  <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+            <h2>التسجيل </h2>
+   
+            <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+            data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+            <div class="features-item2 text-center">
+                <div class="features-icon2">
+                    <img src="assets/images/uni-students-icon.svg" alt="">
+                    <p>الجامعات </p>
+                    <p id="ct">تسجيل متاح <br>12/7/2024 - 5/7/2024</p>
+                    <a href="./forms/Universities.php" class="main-button">سجل</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 col-xs-12"
+            data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+            <div class="features-item2 text-center">
+                <div class="features-icon2">
+                    <img src="assets/images/school-students-icon.svg" alt="">
+                    <p> المدارس</p>
+                    <p id="ct">تسجيل متاح <br>12/7/2024 - 5/7/2024</p>
+                    
+                    <a href="./forms/school.php" class="main-button">سجل</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+                
+                
+            
+               
+              
+            </div>
+        </div>
+    </section>
+  
+<section></section>
+<!-- SOCIAL PANEL HTML -->
+<div class="social-panel-container">
+	<div class="social-panel">
+	
+	</div>
+</div>
+<button  class="floating-btn">
+  <a href="https://wa.me/+96891475573">	 للدعم الفني
+</a>
+</button>
+
+</section>
+
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>جامعة التقنية والعلوم التطبيقية بصلالة ©<br>تطوير: عبدالعزيز المعشني</p>
+
+          
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!---->
+  
+  
+        
+
+
+
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
